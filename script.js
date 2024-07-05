1abd3fccd3bcd35852ffde3682dc84681bd9f595
Md Imtiyaz <md.imtiyaz@in2ittech.com>
2024-07-05 15:12:41 +0530

    added

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 
 
-
+// First Method:
 const card = document.querySelector('.card');
+const front = document.querySelector('.front')
 
 card.addEventListener('click', () => {
     card.classList.toggle('flipped');
+    // front.style.backgroundColor = 'red';
 });
+
+
+
+// Second Method:
+const card1 = document.querySelector('.card');
+const front1 = document.querySelector('.front');
+
+const cardHandlers = () => {
+    card1.classList.toggle('flipped');
+    front.style.backgroundColor = 'red';
+};
+
+// card.addEventListener('click', cardHandlers);
